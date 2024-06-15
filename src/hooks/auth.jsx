88244db25182ad7a29169b7bfbe8d04493c0d55d
Iago Romao <iagoromao@Iagos-Laptop.local>
2024-06-15 11:32:58 -0400

    update

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,15 +1,25 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 import { api } from '../services/api'
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-    async function signIn( email, password){
+
+    const [data, setData] = useState({});
+
+    async function signIn({ email, password }){
         try{
             const response = await api.post("/sessions", { email, password });
-            console.log(response);
-        }catch{
+            const { user, token } = response.data;
+
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token)
+
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({ user, token})
+            console.log(user);
+        }catch (error){
             if(error.response){
                 alert(error.response.data.message);
             }else{
@@ -18,8 +28,23 @@ function AuthProvider({ children }) {
         }
     } 
 
+    // useEffect(() => {
+    //     const token = localStorage.getItem("@rocketnotes:token");
+    //     const user = localStorage.getItem("@rocketnotes:user");
+
+    //     if(token && user) {
+    //         api.defaults.headers.authorization = `Bearer ${token}`;
+
+    //         setData({
+    //             token,
+    //             user: JSON.parse(user)
+    //         });
+    //     }
+
+    // }, []);
+
     return(
-        <AuthContext.Provider value={{ signIn }}>
+        <AuthContext.Provider value={{ signIn, user: data.user }}>
             {children}
         </AuthContext.Provider>
     )
@@ -27,7 +52,7 @@ function AuthProvider({ children }) {
 
 function useAuth(){
     const context = useContext(AuthContext);
-
+    
     return context;
 }
 
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -12,7 +12,7 @@ import { Container, Form, Background } from "./styles";
 export function SignIn() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+ 
     const { signIn } = useAuth();
 
     function handleSignIn(){
